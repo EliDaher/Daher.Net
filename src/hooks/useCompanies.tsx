82a75d96b5c4cf87889b61029fa39caed3201225
elidaher <eeli56315@gmail.com>
2025-12-31 13:33:26 +0300

    fix no company

--- src/hooks/useCompanies.tsx
+++ src/hooks/useCompanies.tsx
@@ -1,12 +1,27 @@
-// hooks/useWarehouses.ts
+// hooks/useCompanies.ts
 import { getAllCompanies } from "@/services/companies";
 import { useQuery } from "@tanstack/react-query";
 
 export function useCompanies() {
-  return useQuery({
+  const query = useQuery({
     queryKey: ["companies-table"],
     queryFn: getAllCompanies,
-    staleTime: 1000 * 60 * 30, // 10 minutes (بدون طلبات جديدة قبل انتهاء الوقت)
+    staleTime: 1000 * 60 * 30,
     refetchOnWindowFocus: false,
+    select: (data) => {
+      // في حال رجعت null أو undefined
+      if (!data) return [];
+
+      // في حال رجعت ليستة فاضية
+      if (Array.isArray(data) && data.length === 0) return [];
+
+      return data;
+    },
   });
+
+  return {
+    ...query,
+    companies: query.data ?? [],
+    isEmpty: !query.isLoading && (query.data?.length ?? 0) === 0,
+  };
 }
--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -57,13 +57,7 @@ export default function Companies() {
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
         >
           {!isLoading && viewMode === "cards" ? (
-            [
-              ...companies,
-              ...companies,
-              ...companies,
-              ...companies,
-              ...companies,
-            ]?.map((company) => (
+            !companies ? [] : companies?.map((company) => (
               <Card
                 dir="rtl"
                 key={company.id}
